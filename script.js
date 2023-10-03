522bbfe3d0e010bf6063d08b017b73008bcfcad4
0xR3TR0 <oliwier@klefas.com>
2023-10-03 22:14:21 +0200

    JavaScript (v.1.1)

--- script.js
+++ script.js
@@ -2,14 +2,27 @@ const passwordBox = document.getElementById("password");
 const lenght = 12;
 
 const upperCase = "ABCDEFGHIJKLMNOPRSTUWXYZ";
-const lowCase = "abcdefghijklmnoprstuwxyz";
+const lowerCase = "abcdefghijklmnoprstuwxyz";
 const number = "0123456789";
 const symbol = "~!@#$%^&*()_+|`-=[]{};:'><,.?/";
+
+const allChars = upperCase + lowerCase + number + symbol;
 
 function createPassword(){
     let password = "";
     password += upperCase[Math.floor(Math.random() * upperCase.length)];
-    password += lowCase[Math.floor(Math.random() * lowCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
     password += number[Math.floor(Math.random() * number.length)];
     password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while(lenght > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+
+    passwordBox.value = password;
+};
+
+function copyPassword(){
+    passwordBox.select();
+    document.execCommand("copy");
 };
